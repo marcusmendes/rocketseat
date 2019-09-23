9831ac437ad65bb4fd3a0eab5c04c5ac4e3e84c3
Marcus Maciel <mendesxx.ti@hotmail.com>
2019-09-23 20:56:34 -0300

    Trabalhando com datas e relacionamentos

--- modulo02/gobarber/src/database/index.js
+++ modulo02/gobarber/src/database/index.js
@@ -2,10 +2,12 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import User from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
 const models = [
   User,
   File,
+  Appointment,
 ];
 
 class Database {
--- modulo02/gobarber/src/routes.js
+++ modulo02/gobarber/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -21,5 +22,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
 
 export default routes;
