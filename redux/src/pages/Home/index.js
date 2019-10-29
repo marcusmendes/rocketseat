b7875a5fcbcc53eda0539e7ea8820bf4d9c1c776
Marcus Maciel <mendesxx.ti@hotmail.com>
2019-10-29 20:54:27 -0300

    Estilizando o carrinho

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-walkabout-wkt-masculino/38/H25-1070-138/H25-1070-138_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-walkabout-wkt-masculino/38/H25-1070-138/H25-1070-138_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/sapatenis-walkabout-wkt-masculino/38/H25-1070-138/H25-1070-138_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+
+        <strong>Tênis muito legal</strong>
+        <span>R$ 129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
