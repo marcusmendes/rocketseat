7610702ea497faca7ecd025f29ddf3b28c628140
Marcus Maciel <marcusmaciel@controllab>
2019-10-31 15:52:24 -0300

    Autenticação

--- modulo02/gobarber-web/src/store/modules/auth/sagas.js
+++ modulo02/gobarber-web/src/store/modules/auth/sagas.js
@@ -1,3 +1,27 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, all, put, call } from 'redux-saga/effects';
+import api from '~/services/api';
+import history from '~/services/history';
 
-export default all([]);
+import { signInSucces } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  if (!user.provider) {
+    console.tron.error('Usuário não é prestador.');
+    return;
+  }
+
+  yield put(signInSucces(token, user));
+
+  history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
